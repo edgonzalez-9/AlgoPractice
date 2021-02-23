69cee8edc632eea1f715da985af8fb6e4cf4d5c8
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-02-23 14:38:29 -0600

    added a second solution to anagrams

--- algopractice/src/exercises/anagrams.js
+++ algopractice/src/exercises/anagrams.js
@@ -28,3 +28,32 @@ function cleanString(str) {
     .sort()
     .join('')
 }
+
+// function anagrams(stringA, stringB) {
+//     //Build character maps and then compare them//
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharMap = buildCharMap(stringB);
+
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//         return false;
+//     }
+
+//     for (let char in aCharMap) {
+//         if (aCharMap[char] !== bCharMap[char]) {
+//             return false;
+//         } else {
+//             return true;
+//         }
+//     } 
+// }
+
+// //Helper Function to make character maps
+// function buildCharMap(str) {
+//     const charMap = {};
+    
+//     for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+
+//     return charMap;
+// }
