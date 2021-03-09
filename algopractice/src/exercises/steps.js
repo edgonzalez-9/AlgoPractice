3ee7450b225f75cbb1deeb564679d85db1eaa7ed
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-03-09 10:05:08 -0600

    added first solution to steps

--- algopractice/src/exercises/steps.js
+++ algopractice/src/exercises/steps.js
@@ -18,5 +18,16 @@
 //       '####'
 
 function steps(n) {
-    
+    for (let row = 0; row < n; row++){
+        let step = '';
+
+        for (let column = 0; column < n; column ++){
+            if (column <= row) {
+                step += '#';
+            } else {
+                step += ' ';
+            }
+        }
+        console.log(step)
+    }
 }
